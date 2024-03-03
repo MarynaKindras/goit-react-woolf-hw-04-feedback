d9cbfe9d687ebc2a6ad64f07423f11b2f8cff0ae
MarynaKindras <maryna.kindras@gmail.com>
2024-03-03 15:08:03 +0200

    Main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
+import React from 'react';
+import { useState } from 'react';
+
+import { FeedbackOptions } from './Feedback/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+import { Statistics } from './Statistics/Statistics';
+
+import { Wrapper } from './App.styled';
+
 export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = pressedBtn => {
+    switch (pressedBtn) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total > 0 ? ((good / total) * 100).toFixed(0) : 0;
+  };
+
+  const total = countTotalFeedback();
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Wrapper>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
+          />
+        )}
+      </Section>
+    </Wrapper>
   );
 };
---
+++ src/components/App.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 500px;
+  height: 100vh;
+  diaply: flex;
+  flex-direction: column;
+  padding: 20px;
+  color: #010101;
+`;
